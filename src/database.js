6558e96fe0c7f87601e139dc160ad7bdb220791b
cristianaquino <ycristian10@gmail.com>
2022-12-07 21:02:30 -0500

    database

--- src/database.js
+++ src/database.js
@@ -1,11 +1,12 @@
-import mongoose from "mongoose";
+dotenv.config();
 import dotenv from "dotenv";
+import mongoose from "mongoose";
 
 const { DATA_BASE_URI } = process.env;
 
-dotenv.config();
-
-mongoose
+const database = mongoose
   .connect(DATA_BASE_URI)
   .then(() => console.log("conectado"))
-  .catch((error) => console.error(error));
+  .catch((error) => console.log(error));
+
+export default database;
