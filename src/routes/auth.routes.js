6e2ff3e11610020b3d37dc1a6b6f44004c6cf4db
cristianaquino <ycristian10@gmail.com>
2022-12-08 16:11:48 -0500

    auth

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,9 +1,10 @@
 import { Router } from "express";
 import { registerUser, loginUser } from "../controllers/auth.controller.js";
+import { checkDuplicateUsername } from "../middlewares/verifyUser.js";
 
 const router = Router();
 
-router.post("/register", registerUser);
+router.post("/register", checkDuplicateUsername, registerUser);
 router.post("/login", loginUser);
 
 export default router;
