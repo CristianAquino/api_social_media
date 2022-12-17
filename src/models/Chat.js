05fa8923473fccd9971c5763b3af916f38551d92
cristianaquino <ycristian10@gmail.com>
2022-12-17 01:03:27 -0500

    chat

---
+++ src/models/Chat.js
@@ -0,0 +1,20 @@
+import { Schema, model } from "mongoose";
+
+const chatSchema = new Schema(
+  {
+    members: {
+      type: Array,
+    },
+  },
+  { timestamps: true, versionKey: false }
+);
+
+postSchema.set("toJSON", {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id;
+    delete returnedObject._id;
+  },
+});
+
+const chatModel = model("Chat", chatSchema);
+export default chatModel;
---
+++ src/routes/chat.routes.js
@@ -0,0 +1,15 @@
+import { Router } from "express";
+import {
+  createChat,
+  findChat,
+  userChats,
+} from "../controllers/chat.controller.js";
+import { verifyToken } from "../middlewares/authJWT.js";
+
+const router = Router();
+
+router.post("/", verifyToken, createChat);
+router.get("/:userId", verifyToken, userChats);
+router.get("/find/:firstId/:secondId", verifyToken, findChat);
+
+export default router;
